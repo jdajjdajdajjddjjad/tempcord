02629bad7ef45870906d81fb5d818c342ce8fd13
Cyrus Chan <cyruschan1212@gmail.com>
2022-03-30 19:30:32 +0800

    SQLite implementation

---
+++ lib/database.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+Database? _profileRecordDB = null;
+
+Future<Database> get profileRecordDB async {
+  if (_profileRecordDB != null && _profileRecordDB!.isOpen) {
+    // Skip open database action if is opened
+    return _profileRecordDB!;
+  }
+
+  String databasePath = path.join(
+      await (Platform.isAndroid || Platform.isIOS
+          ? getDatabasesPath()
+          : path_provider
+              .getApplicationSupportDirectory()
+              .then((dir) => dir.path)),
+      "profile_record.db");
+
+  _profileRecordDB =
+      await openDatabase(databasePath, version: 1, onConfigure: (db) async {
+    await db.execute("PRAGMA foreign_keys = ON");
+  }, onCreate: (db, version) async {
+    await db.execute(
+        "CREATE TABLE profiles (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, animal TEXT NOT NULL, image BLOB)");
+    await db.execute(
+        "CREATE TABLE btr (id INTEGER PRIMARY KEY AUTOINCREMENT, pid INTEGER NOT NULL, temp REAL NOT NULL, unit TEXT NOT NULL, recordedAt TEXT NOT NULL, FOREIGN KEY (pid) REFERENCES profiles (id) ON UPDATE RESTRICT ON DELETE RESTRICT");
+  });
+
+  return _profileRecordDB!;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
-import 'package:fluent_ui/fluent_ui.dart';
+import 'dart:io';
 
-// Pending
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+import 'database.dart' as tempcord_db;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // SQLite binding
+  await _sqlInit();
+}
+
+Future<void> _sqlInit() async {
+  if (Platform.isLinux || Platform.isWindows) {
+    sqfliteFfiInit();
+  }
+
+  databaseFactory = databaseFactoryFfi;
+
+  await tempcord_db.profileRecordDB.then((db) async => await db.close());
 }
